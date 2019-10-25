3a6881dde4cdbde51c2cb23a8f55d5584c42c7d8
Aidan Kelly <it.is.aidan@gmail.com>
2019-10-24 20:52:07 -0600

    Added code

---
+++ basic_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import socket
+import threading
+
+#basic socket setup
+OUR_IP = '127.0.0.1'
+OUR_PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((OUR_IP, OUR_PORT))
+server_socket.listen(5)
+
+print(f"[*] Listening on {OUR_IP}:{OUR_PORT}")
+
+#vars for number of acks, and number of connections
+ackNum = 0
+numConnection = 0
+clients = []
+
+def handleClient(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        print(message)
+        for c in clients:
+            if c.getpeername() != client_socket.getpeername():
+                #send back an ack with an id
+                #ack = f"ACK{ackNum}!"
+                message = f"{c.getpeername()}> {message}"
+                c.send(bytes(message, 'utf-8'))
+
+
+#main server loop
+while True:
+
+    #accepts one connection for testing purposes
+    if numConnection != 2:
+        client, addr = server_socket.accept()
+        numConnection +=1
+        print(f"[*] New connection from {addr[0]}:{addr[1]}")
+        clients.append(client)
+
+        handleClientThread = threading.Thread(target=handleClient, args=(client,))
+        handleClientThread.start()
+
+    # #recieve message from client
+    # message = client.recv(1024).decode('utf-8')
+    # print(message)
+
+    # for c in clients:
+    #     if c != client:
+    #         #send back an ack with an id
+    #         ack = f"ACK{ackNum}!"
+    #         client.send(bytes(ack, 'utf-8'))
+    #         ackNum += 1
---
+++ client_gui.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+from tkinter import *
+import sys
+import socket
+import threading
+
+###
+### TODO need to implement another thread to accept incoming data from the server
+###      will need to display this data within the console window. 
+###
+
+#vars for width and height.
+HEIGHT = 700
+WIDTH = 1000
+
+#basic socket setup
+TARGET_IP = '127.0.0.1'
+TARGET_PORT = 1234
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((TARGET_IP, TARGET_PORT))
+
+#set up our base window
+root=Tk()
+root.title("Chatroom")
+root.geometry(f"{WIDTH}x{HEIGHT}")
+
+#set up the console window
+textbox=Text(root, bg='#080808', fg='#00BF20')
+textbox.pack(fill=BOTH, expand=True)
+
+#set up the input field
+ourInput = Entry(root)
+ourInput.pack(fill=X, side=BOTTOM)
+ourInput.focus_set()
+
+#create a seperate thread that will receive data
+def receiveDataThread(client_socket):
+    while True:
+        msg = client_socket.recv(1024).decode('utf-8')
+        print(msg)
+
+#This function will send off data
+def returnPressed(event):
+
+    #grab the input
+    enterdText = ourInput.get()
+
+    #check to see if user wants to exit
+    if enterdText == "exit":
+        sys.exit()
+    
+    #output the text
+    print(f"YOU> {enterdText}")
+
+    #send the data to the server
+    client_socket.send(bytes(enterdText, 'utf-8'))
+    ###
+    ### TODO in future, need to check this data before we send it to the server
+    ###
+
+    #ensure that the last entry in client is shown
+    textbox.see("end")
+
+    #clear the input field
+    ourInput.delete(0, END)
+    
+
+#makes it so data is sent on pressing of RETURN key
+root.bind("<Return>", returnPressed)
+
+#makes it so that any print statements are printed to the textbox
+def redirector(inputStr):
+    textbox.insert(INSERT, inputStr)
+
+#on print inserts data to text box
+sys.stdout.write = redirector #whenever sys.stdout.write is called, redirector is called.
+
+receiveThread = threading.Thread(target=receiveDataThread, daemon=True, args=(client_socket,))
+receiveThread.start()
+
+#start up the GUI
+root.mainloop()
